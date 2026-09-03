7e68764c242c18301afd51aea30966e10a0a3442
Mark Melo <93807422+mmarkandrei@users.noreply.github.com>
2026-09-03 22:30:28 +0800

    Add input-output script for item pricing

---
+++ Input-Output:1stCase.py
@@ -0,0 +1,14 @@
+print("Item Item price")
+print("Paper 50/pad")
+print("Pencil 10/each")
+print("Ballpen 20/each")
+print("Bond Paper 5/each")
+print("Glue 25/each")
+
+name=input("Enter item name=")
+price=int(input("Enter item price="))
+quantity=int(input("Enter quantity="))
+
+total=price*quantity
+
+print("Total=",total)
